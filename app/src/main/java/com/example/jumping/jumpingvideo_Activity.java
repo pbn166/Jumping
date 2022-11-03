d101134ade4b6d98e3c3859b51e6d3a4a45da7e6
pbn166 <nhib1906335@student.ctu.edu.vn>
2022-11-03 23:39:35 +0700

    Updaté video

---
+++ app/src/main/java/com/example/jumping/jumpingvideo_Activity.java
@@ -0,0 +1,14 @@
+package com.example.jumping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jumpingvideo_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jumpingvideo_layout);
+    }
+}
